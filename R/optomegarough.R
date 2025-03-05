f861b70f3c5d79b44fc6f3ef475d53f555a94968
brownegm <32085030+brownegm@users.noreply.github.com>
2025-03-04 17:35:01 -0800

    added silent option to retVOD

--- R/optomegarough.R
+++ R/optomegarough.R
@@ -11,10 +11,15 @@
 #' @importFrom tidyselect contains
 
 optOR <- function(df, range_omega, range_roughness, precision) {
+
   optOut <- vector("list", length(range_omega) * length(range_roughness))
   names(optOut) <- character(length(optOut))
 
   index <- 1
+  print(range_omega)
+  print(range_roughness)
+
+  cli::cli_progress_bar("Optimizing omega and roughness...")
   for (o in seq_along(range_omega)) {
     for (r in seq_along(range_roughness)) {
       retrieval <- retVOD(
@@ -24,21 +29,25 @@ optOR <- function(df, range_omega, range_roughness, precision) {
         Tsoil = df[["mean_soil_temp"]],
         omega = range_omega[o],
         roughness = range_roughness[r],
-        inc_angle = 40
+        inc_angle = 40, silent = T
       )
 
-      name <- paste("omega", round(o, 5), "rough", r, sep = "_")
+      name <- paste("omega", round(range_omega[o], 5), "rough", range_roughness[r], sep = "_")
       print(name)
       optOut[[index]] <- sum(retrieval$costfx, na.rm = T)
       names(optOut)[index] <- name
       index <- index + 1
+
+      cli::cli_progress_update()
     }
   }
-
+  cli::cli_progress_done()
   # Convert results list to dataframe
   optimization_df <- do.call(cbind, optOut) |>
     as.data.frame() |>
-    tidyr::pivot_longer(cols = tidyselect::contains("omega"), names_to = "omega_roughness", values_to = "cf")
+    tidyr::pivot_longer(cols = tidyselect::contains("omega"),
+                        names_to = "omega_roughness",
+                        values_to = "cf")
 
   # Find best parameters
   best_params <- optimization_df[which.min(optimization_df$cf), ]
--- R/retVOD.R
+++ R/retVOD.R
@@ -9,6 +9,7 @@
 #' @param omega Canopy opacity, scattering albedo
 #' @param roughness Soil roughness estimate
 #' @param inc_angle Incidence angle
+#' @param silent Silence progress bars in the console. Default is FALSE.
 #'
 #' @return Retrieved VOD and auxillary information
 #' @export
@@ -22,9 +23,8 @@ retVOD <- function(tbH, tbV,
                    Tsoil,
                    omega,
                    roughness,
-                   inc_angle) {
-
-  if (length(tbH)!= length(tbV)|| length(tbH)!= length(smc)) {
+                   inc_angle, silent = F) {
+  if (length(tbH) != length(tbV) || length(tbH) != length(smc)) {
     stop("tbH, tbV, and smc lengths differ.")
   }
 
@@ -35,12 +35,14 @@ retVOD <- function(tbH, tbV,
   results <- vector("list", length(tbH))
   names(results) <- c(
     "vodEst", "cfEst", "smEst",
-     "tbHpred","tbVpred",
+    "tbHpred", "tbVpred",
     "tbHcost", "tbVcost"
   )
 
   # for each brightness temperature retrieve VOD and soil moisture
-  cli::cli_progress_bar("Retrieving VOD values...", total = length(tbH))
+  if (silent == F) {
+    cli::cli_progress_bar("Retrieving VOD values...", total = length(tbH))
+  }
   for (i in seq_along(tbH)) {
     est <- solveSmVod(
       smc = smc[i], tbH = tbH[i], tbV = tbV[i],
@@ -58,22 +60,24 @@ retVOD <- function(tbH, tbV,
     results$tbHpred[i] <- est$pred_tbH
     results$tbHcost[i] <- est$cf_tbH
     results$tbVcost[i] <- est$cf_tbV
-
-    cli_progress_update()  # Update progress bar
+    if (silent == F) {
+      cli_progress_update() # Update progress bar
+    }
   }
 
-  results$rmse <- mean(sqrt(results$cfEst), na.rm=T)
+  results$rmse <- mean(sqrt(results$cfEst), na.rm = T)
 
-  cli_progress_done()  # Complete progress bar
-  cli_alert_success("VOD retrieval complete!")
-
+  if (silent == F) {
+    cli_progress_done() # Complete progress bar
+    cli_alert_success("VOD retrieval complete!")
+  }
   if (!identical(results$smEst, smc)) {
     warning("'Estimated' soil moisture values do not match input.")
   }
 
   return(structure(results,
     creation_time = Sys.time(),
-    inputs = list(h=tbH, v=tbV, sm = smc, omega = omega, rough = roughness,angle =inc_angle),
+    inputs = list(h = tbH, v = tbV, sm = smc, omega = omega, rough = roughness, angle = inc_angle),
     class = "retVOD"
   ))
 }
@@ -82,47 +86,61 @@ retVOD <- function(tbH, tbV,
 #' @export
 #' @importFrom graphics hist par abline
 #' @importFrom grDevices dev.new
-plot.retVOD <- function(x, ...){
-
+plot.retVOD <- function(x, ...) {
   dev.new(width = 10, height = 6)
 
   inputs <- attr(x, "inputs")
-  par(mfrow = c(3,3), mar = c(5, 5, 1, 1))
+  par(mfrow = c(3, 3), mar = c(5, 5, 1, 1))
 
-  plot(x$vodEst, main = "Retrieved Vegetation Optical Depth",
-       ylab = "VOD",
-       xlab = "Index")
+  plot(x$vodEst,
+    main = "Retrieved Vegetation Optical Depth",
+    ylab = "VOD",
+    xlab = "Index"
+  )
 
-  plot(x$cfEst, main = "Total Tb Residuals",
-       ylab = "Brightness Temps (K^2)",
-       xlab = "Index")
+  plot(x$cfEst,
+    main = "Total Tb Residuals",
+    ylab = "Brightness Temps (K^2)",
+    xlab = "Index"
+  )
 
-  hist(x$cfEst, main = "Histogram Tb Residuals", xlab= "Tb Residuals")
+  hist(x$cfEst, main = "Histogram Tb Residuals", xlab = "Tb Residuals")
 
-  plot(x$tbHcost, main = "TbH Residuals (K^2)",
-       ylab = "Brightness Temps (K^2)",
-       xlab = "Index")
+  plot(x$tbHcost,
+    main = "TbH Residuals (K^2)",
+    ylab = "Brightness Temps (K^2)",
+    xlab = "Index"
+  )
 
-  plot(x$tbVcost, main = "TbV Residuals (K^2)",
-       ylab = "Brightness Temps (K^2)",
-       xlab = "Index")
+  plot(x$tbVcost,
+    main = "TbV Residuals (K^2)",
+    ylab = "Brightness Temps (K^2)",
+    xlab = "Index"
+  )
 
-  plot(x$tbHpred ~ x$tbHcost, main = "Predicted TbH ~ TbH Residuals",
-       ylab = "Brightness Temps (K)",
-       xlab = "Tb H Residuals (K^2)")
+  plot(x$tbHpred ~ x$tbHcost,
+    main = "Predicted TbH ~ TbH Residuals",
+    ylab = "Brightness Temps (K)",
+    xlab = "Tb H Residuals (K^2)"
+  )
   abline(a = 0, b = -1, col = "red")
-  plot(x$tbVpred ~ x$tbVcost, main = "Predicted TbV ~ TbV Residuals",
-       ylab = "Brightness Temps (K)",
-       xlab = "Tb V Residuals (K^2)")
+  plot(x$tbVpred ~ x$tbVcost,
+    main = "Predicted TbV ~ TbV Residuals",
+    ylab = "Brightness Temps (K)",
+    xlab = "Tb V Residuals (K^2)"
+  )
   abline(a = 0, b = -1, col = "red")
 
-  plot(inputs$h ~ x$tbHcost, main = "Observed TbH ~ TbH Residuals",
-       ylab = "Brightness Temps (K)",
-       xlab = "Tb H Residuals (K^2)")
+  plot(inputs$h ~ x$tbHcost,
+    main = "Observed TbH ~ TbH Residuals",
+    ylab = "Brightness Temps (K)",
+    xlab = "Tb H Residuals (K^2)"
+  )
   abline(a = 0, b = 1, col = "red")
-  plot(inputs$v ~ x$tbVcost, main = "Observed TbV ~ TbV Residuals",
-       ylab = "Brightness Temps (K)",
-       xlab = "Tb V Residuals (K^2)")
+  plot(inputs$v ~ x$tbVcost,
+    main = "Observed TbV ~ TbV Residuals",
+    ylab = "Brightness Temps (K)",
+    xlab = "Tb V Residuals (K^2)"
+  )
   abline(a = 0, b = 1, col = "red")
-
 }
